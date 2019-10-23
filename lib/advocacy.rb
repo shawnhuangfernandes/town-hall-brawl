a365346c536ebc8c7f71bb38a59828cd3a8388ca
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-10-22 17:01:21 -0700

    I don't know if my commits are working

--- lib/advocacy.rb
+++ lib/advocacy.rb
@@ -1,4 +1,12 @@
 class Advocacy < ActiveRecord::Base
     belongs_to :citizen
     belongs_to :initiative
+
+    def initiative
+        Initiative.find(self.initiative_id)
+    end
+
+    def initiative=(initiative)
+        self[:initiative_id] = initiative.id
+    end
 end
--- lib/citizen.rb
+++ lib/citizen.rb
@@ -1,15 +1,37 @@
 class Citizen < ActiveRecord::Base
-    has_many :advocacies
-    has_many :initiatives, through: :advocacies
+    has_many :advocacies # relationship - a citizen has many advocacies
+    has_many :initiatives, through: :advocacies # relationship - a citizen has many initiatives through advocacies
 
+    # creates two new advocacy associations linked to random initatives and assigns them to this citizen
     def supportTwoRandomInitiatives
+        list_of_ids = Initiative.all.map {|initiative| initiative.id} # get a list of ids for all the initiatives
 
-        list_of_ids = Initiative.all.map {|initiative| initiative.id}
-
-        init_id1 = list_of_ids.sample
+        init_id1 = list_of_ids.sample # get a random id for the first initative to assign
+        init_id2 = init_id1 # set the second id to the first id (to pass the condition below)
         
+        # until the second initiative is not the same (id) as the first initiative
+        until init_id2 != init_id1 do 
+            init_id2 = list_of_ids.sample # randomize the second initiative
+        end
 
-        Advocacy.new(self, Initiative.find)
-    
+        # take two associations (one for both initiatives and this citizen)
+        Advocacy.create(citizen: self, initiative: Initiative.find(init_id1))
+        Advocacy.create(citizen: self, initiative: Initiative.find(init_id2))  
+    end
+
+    def self.Brawl(citizen_array)
+        if Advocacy.all.map {|advocacy| advocacy.initiative.name}.uniq.size == 2 # we only have two initiatives in play
+            return citizen_array
+        else 
+            citizens_in_brawl = citizen_array.shuffle
+            binding.pry
+        end# else, lets make people fight
+            # citizens_in_brawl = citizen_array.shuffle # lets shuffle the citizens who are alive
+            # citizen_of_focus = citizens_in_brawl[0] # lets grab the first citizen
+            # citizens_in_brawl.each do |citizen| # make the citizen citizen_of_focus fight the first citizen with dissimilar values
+            #     if (citizen.initiatives == citizen[0].initiatives)
+            #         binding.pry
+            #     end
+            # end
     end
 end
